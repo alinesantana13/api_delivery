265423cc4f4d9952df217e201eaeabc5fae3ccfb
Aline Santana <aline_santana13@hotmail.com>
2024-03-03 14:41:15 -0300

    Cria guia para automatizar testes de API (json).

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_length_of(:name).is_at_least(3) }
+    # it "should be valid when name is filled" do
+    #   store = Store.new
+    #   expect(store).to_not be_valid
+    # end
+    # it "should be valid when name is filled" do
+    #   store = Store.new name: ""
+    #   expect(store.valid?).to eq false
+    # end
+  end
 end
